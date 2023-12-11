819d268233c308561a330f7cdcc8c8ad14d47d26
ilhan <ilhan@ILHAN_KERTMEN>
2023-12-11 20:14:40 +0300

    Add project files.

---
+++ RedisEntegration.Business/Interfaces/IRedisCacheServices.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace RedisEntegration.Business.Interfaces
+{
+    public interface IRedisCacheServices
+    {
+        Task ClearAsync(string key);
+        Task<string> GetValueAsync(string key);
+        Task<bool> SetValueAsync(string key, string value);
+        void ClearAll();
+    }
+}
---
+++ RedisEntegration.Business/Services/RedisCacheServices.cs
@@ -0,0 +1,47 @@
+﻿using RedisEntegration.Business.Interfaces;
+using StackExchange.Redis;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace RedisEntegration.Business.Services
+{
+    public class RedisCacheServices : IRedisCacheServices
+    {
+        private readonly IConnectionMultiplexer _connectionMultiplexer;
+        private readonly IDatabaseAsync _databaseAsync;
+
+        public RedisCacheServices(IConnectionMultiplexer connectionMultiplexer)
+        {
+            _connectionMultiplexer = connectionMultiplexer;
+            _databaseAsync = _connectionMultiplexer.GetDatabase();
+        }
+
+        public async Task ClearAsync(string key)
+        {
+            await _databaseAsync.KeyDeleteAsync(key);
+        }
+
+        public async Task<string> GetValueAsync(string key)
+        {
+            return await _databaseAsync.StringGetAsync(key);
+        }
+
+        public async Task<bool> SetValueAsync(string key, string value)
+        {
+            return await _databaseAsync.StringSetAsync(key, value);
+        }
+
+        public void ClearAll()
+        {
+            var redisEndpoints = _connectionMultiplexer.GetEndPoints(true);
+            foreach (var redisEndpoint in redisEndpoints)
+            {
+                var redisServer = _connectionMultiplexer.GetServer(redisEndpoint);
+                redisServer.FlushAllDatabases();
+            }
+        }
+    }
+}
